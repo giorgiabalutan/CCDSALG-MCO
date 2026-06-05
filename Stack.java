0b31e34e952127336f71189cda21df386ee40825
GildedEternal <gildedtriplea@email.com>
2026-06-05 21:40:18 +0800

    Added Libraries

---
+++ Stack.java
@@ -0,0 +1,117 @@
+public class Stack
+{
+    int stack[];
+    int top = 0;
+    int size;
+
+    //Constructor Method for creating the stack with size input
+    public Stack(int setSize)
+    {
+        this.stack = new int[setSize];
+        this.size = setSize;
+    }
+
+    //Checks if the stack is empty
+    public boolean stackEmpty()
+    {
+        return (top == 0);
+    }
+
+    //Checks if the stack is full
+    public boolean stackFull()
+    {
+        return (top == size);
+    }
+
+    //Adds an element to the top of the stack
+    public void push(int element)
+    {
+        //If the stack is full, throws an exception that can be caught in the main program using catch
+        if(this.stackFull()){
+            throw new IllegalStateException("Can't push, the stack is currently full");
+        }else{
+            //Adds the element
+            this.stack[top] = element;
+            top++;
+        }
+    }
+
+    //Removes an element from the top of the stack
+    public int pop()
+    {
+        //If the stack is empty, throws an exception that can be caught in the main program using catch
+        if(this.stackEmpty()){
+            throw new IllegalStateException("Can't pop, the stack is currently empty");
+        }else{
+            //Removes and Returns the element
+            top--;
+            int ret = this.stack[top];
+            this.stack[top] = 0;
+            return ret;
+        }
+    }
+
+    //Returns the top element without removing it
+    public int top()
+    {
+        //If the stack is empty, throws an exception that can be caught in the main program using catch
+        if(this.stackEmpty()){
+            throw new IllegalStateException("Nothing to return, the stack is currently empty");
+        }else{
+            //Returns the element
+            return this.stack[top-1];
+        }
+        
+    }
+
+    //Prints the elements in the stack from the bottom to the top
+    public void show()
+    {
+        System.out.print("Bottom |");
+        for (int i = 0; i < top; i++)
+        {
+            System.out.print(this.stack[i]);
+            System.out.print("|");
+        }
+        System.out.println(" Top");
+    }
+    
+    //Prints the elements including empty spaces (which are 0 for integers by default in java)
+    public void showWithEmpty()
+    {
+        System.out.print("Bottom |");
+        for (int i = 0; i < size; i++)
+        {
+            System.out.print(this.stack[i]);
+            System.out.print("|");
+        }
+        System.out.println(" Top");
+    }
+
+    public Stack remove(Stack S, int n)
+    {
+        Stack tempS1 = new Stack(20);
+        Queue tempQ1 = new Queue(20);
+        int x1;
+        int j;
+
+        j = 0;
+
+        while (j<n-1)
+        {
+            x1 = S.pop();
+            tempS1.push(x1);
+            j++;
+        }
+
+        S.pop();
+
+        while (!tempS1.stackEmpty())
+        {
+            x1 = tempS1.pop();
+            S.push(x1);
+        }
+
+        return S;
+    }
+}
