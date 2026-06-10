fe91c21ef53715e2f73f650fc65d886d73dab7f0
Giorgia <giorgiabalutan@gmail.com>
2026-06-10 09:36:54 +0800

    Driver class tests out methods (creds to Aaron)

--- Driver.java
+++ Driver.java
@@ -1,6 +1,16 @@
 public class Driver
 {
     public static void main(String[] args) {
+        Token[] eq = new Token[] {
+            new Token(1),
+            new Token('+'),
+            new Token(2)
+        };
+        Equation equation = new Equation(eq,3);
         
+        equation.printEq();
+        equation.convertInfixToPrefix();
+        System.out.println();
+        equation.printEq();
     }
 }
